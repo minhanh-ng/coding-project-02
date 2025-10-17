3a88c17c9797085943e299997f8326db5ab286f4
Minh Anh Nguyen <minhanhn@usf.edu>
2025-10-17 14:29:07 -0400

    used try/catch and async/wait to retrieve product data

--- product-dashboard-vanilla-js/cp_2.js
+++ product-dashboard-vanilla-js/cp_2.js
@@ -9,5 +9,17 @@ function fetchProductsThen() {
     .catch((err) => {
         console.error("Fetchfailed:", err);
     })
-   
+};
+
+//Step 4: fetchProductsAsync(), use async/await and try/catch to retrieve product data, displayProducts(products), and handleError(error)
+async function fetchProductsAsync() {
+    try{
+        const res = await fetch("https://www.course-api.com/javascript-store-products");
+        const data = await res.json();
+        displayProduct(data);
+        return data;
+    } catch (err) {
+        handleError(err);
+        return null;
+    }
 }
